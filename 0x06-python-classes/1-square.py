89b9b13f0f11bd359cbf2208cbcc75e1a7b59606
Akiyafaith <akiyafaith@gmail.com>
2023-03-28 09:59:10 -0700

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""define a class"""
+
+class Square:
+    """
+    A class that represents a square.
+
+    Attributes:
+        - size (int): The size of each side of the square.
+
+    Methods:
+        - __init__(self, size): Initializes a new Square object with the given size.
+    """
+
+    def __init__(self, size):
+        """
+        Initializes a new Square object with the given size.
+
+        Parameters:
+        - size (int): The size of each side of the square.
+        """
+        self.__size = size
+
