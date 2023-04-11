9ebdc5fe7ac820e063143ded88c8b3fe9583f83c
Akiyafaith <akiyafaith@gmail.com>
2023-04-11 14:15:46 -0700

    task 0

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -6,6 +6,6 @@ def lookup(obj):
     """returns the list of available attributes and methods of an object"""
     methods = []
     for att in dir(obj):
-        if not att.startswith("_"):
+        if not att.startswith("__"):
             methods.append(att)
     return methods
