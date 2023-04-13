2a7ee524fffb26963a8fb6054e3d09a9a8680834
Akiyafaith <akiyafaith@gmail.com>
2023-04-13 03:09:13 -0700

    task 5

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -9,7 +9,4 @@ def save_to_json_file(my_obj, filename):
         with open(filename, 'w') as fn:
             json.dump(my_obj, fn)
     except TypeError as e:
-        if isinstance(my_obj, set):
-            raise TypeError(f"{my_obj} is not JSON serializable")
-        else:
-            raise TypeError(f"{e} is not JSON serializable")
+        raise TypeError(f"{my_obj} is not JSON serializable")
