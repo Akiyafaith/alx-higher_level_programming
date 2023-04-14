feb1bfcf2ed9655eea5bf803cdfc0a68d1fc1951
Akiyafaith <akiyafaith@gmail.com>
2023-04-13 17:32:56 -0700

    task 3

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -5,8 +5,5 @@ using a JSON representation"""
 
 
 def save_to_json_file(my_obj, filename):
-    try:
-        with open(filename, 'w') as fn:
-            json.dump(my_obj, fn)
-    except TypeError as e:
-        raise TypeError(f"{my_obj} is not JSON serializable")
+    with open(filename, 'w') as fn:
+        json.dump(my_obj, fn)
