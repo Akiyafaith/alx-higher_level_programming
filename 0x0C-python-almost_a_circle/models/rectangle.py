340387575db16f79b94f61b36ea89dfad71838d6
akiyafaith <akiyafaith@gmail.com>
2023-04-17 04:00:37 -0700

    task 6

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -77,3 +77,6 @@ class Rectangle(Base):
             raise ValueError("y must be >= 0")
         else:
             self.__y = value
+    def __str__(self):
+        """returns [Rectangle] (<id>) <x>/<y> - <width>/<height> representation"""
+        return "[Rectangle] ({}) {}/{} - {}/{}". format(self.id, self.x, self.y, self.width, self.height)
