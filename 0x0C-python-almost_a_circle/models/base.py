ca8a4790d466c5b4140f0cf206f5e37a95218cfe
akiyafaith <akiyafaith@gmail.com>
2023-04-17 13:46:18 -0700

    task 1

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -14,21 +14,24 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
     def to_json_string(list_dictionaries):
-        """returns the JSON string representation of list_dictionaries,
-        Otherwise, return the JSON string representation of list_dictionaries"""
+        """returns the JSON string representation of list_dictionaries"""
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
-    """define a class method that writes the JSON string representation of list_objs to a file"""
+    """define a class method that writes the JSON string
+    representation of list_objs to a file"""
     @classmethod
     def save_to_file(cls, list_objs):
         if list_objs is None:
             list_objs = []
         fname = cls.__name__ + ".json"
         with open(fname, "w") as fn:
-            fn.write(cls.to_json_string([obj.to_dictionary() for obj in list_objs]))
-    
+            fn.write(cls.to_json_string(
+                [obj.to_dictionary() for obj in list_objs]
+                ))
+
     @staticmethod
     def from_json_string(json_string):
         """returns the list of the JSON string representation json_string"""
@@ -45,3 +48,16 @@ class Base:
             dummy = cls(1)
         dummy.update(**dictionary)
         return dummy
+
+    def load_from_file(cls):
+        """returns a list of instances"""
+        fn = "{}.json".format(cls.__name__)
+        try:
+            with open(fn, mode="r", encoding="utf-8") as f:
+                objs_json = cls.from_json_string(f.read())
+                objs = []
+                for obj in objs_json:
+                    objs.append(cls.create(**obj))
+                return objs
+        except FileNotFoundError:
+            return []
