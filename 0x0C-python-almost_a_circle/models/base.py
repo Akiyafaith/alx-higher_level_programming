d7ad9cbb5290cc815c241b88ae0f434a33113971
akiyafaith <akiyafaith@gmail.com>
2023-04-21 15:41:56 -0700

    task 1

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+"""import a module"""
 import json
 """define a class Base"""
 
 
 class Base:
+    """define a variable for the class Base"""
     __nb_objects = 0
 
     """initialize an instance of the class Base"""
@@ -15,11 +17,14 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    """define a class method that takes the list of dictionaries"""
+    @classmethod
     def to_json_string(list_dictionaries):
         """returns the JSON string representation of list_dictionaries"""
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
+
     """define a class method that writes the JSON string
     representation of list_objs to a file"""
     @classmethod
@@ -32,6 +37,8 @@ class Base:
                 [obj.to_dictionary() for obj in list_objs]
                 ))
 
+    """define a class method that checks if the string 
+    is empty"""
     @staticmethod
     def from_json_string(json_string):
         """returns the list of the JSON string representation json_string"""
@@ -39,6 +46,7 @@ class Base:
             return []
         return json.loads(json_string)
 
+    """define a class method"""
     @classmethod
     def create(cls, **dictionary):
         """returns an instance with all attributes already set"""
@@ -49,6 +57,7 @@ class Base:
         dummy.update(**dictionary)
         return dummy
 
+    """define a class method"""
     def load_from_file(cls):
         """returns a list of instances"""
         fn = "{}.json".format(cls.__name__)
@@ -58,3 +67,8 @@ class Base:
                 return [cls.create(**dict) for dict in list_dicts]
         except FileNotFoundError:
             return []
+
+    """define a class method"""
+    def to_dictionary(self):
+        """returns the dictionary representation of the rectangle"""
+        return {"id": self.id, 'width': self.width, 'height': self.height,'x': self.x, 'y': self.y}
