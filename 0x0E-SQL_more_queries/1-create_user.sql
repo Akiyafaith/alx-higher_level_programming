8b9563f1e8ccdfab50a4b68668be8c43d6ab1e17
Akiyafaith <akiyafaith@gmail.com>
2023-06-06 11:51:21 -0700

    create user

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- A script that creates a user
+-- grant the user all privileges and set password
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
