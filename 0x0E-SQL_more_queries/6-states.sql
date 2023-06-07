32fdb4008a0a6ac1316ac7f920ad5069fe9c211b
Akiyafaith <akiyafaith@gmail.com>
2023-06-07 03:25:26 -0700

    states

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,8 +1,9 @@
 -- create a database and a table
+
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
 CREATE TABLE IF NOT EXISTS states (
 	id INT NOT NULL AUTO_INCREMENT,
 	name VARCHAR(256) NOT NULL,
 	PRIMARY KEY (id)
-	)
+	);
