3f412ab0d6bf355395c321cc10f677a6eaf3428f
Akiyafaith <akiyafaith@gmail.com>
2023-06-07 05:21:35 -0700

    cities

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,6 @@
 -- list all cities found in the db
 -- sort results in asc order
 
-SELECT * FROM cities
+SELECT id, name FROM cities
 WHERE state_id IN (SELECT id FROM states WHERE name = 'California')
 ORDER BY id ASC;
