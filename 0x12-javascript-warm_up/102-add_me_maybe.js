dc6e1aa9b0e5064933b8debb65f9a8ac515cb60a
Akiyafaith <akiyafaith@gmail.com>
2023-06-12 16:19:07 -0700

    update

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
-function addMeMaybe(number, theFunction) {
+function addMeMaybe (number, theFunction) {
   number++;
   theFunction(number);
 }
 
 module.exports = {
-	addMeMaybe: addMeMaybe
+  addMeMaybe
 };
