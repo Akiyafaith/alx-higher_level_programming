8aced0b55ce1861403f2e25fd4402ceacf5b51e4
akiyafaith <akiyafaith@gmail.com>
2023-07-04 14:10:33 -0700

    updated

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,11 +5,12 @@ import urllib.request
 
 
 if __name__ == "__main__":
-url = 'https://alx-intranet.hbtn.io/status'
+    #get a URL
+    url = 'https://alx-intranet.hbtn.io/status'
 
-with urllib.request.urlopen(url) as response:
+    with urllib.request.urlopen(url) as response:
     body = response.read()
-print("Body response:")
-print("\t- type:", type(body))
-print("\t- content:", body)
-print("\t- utf8 content:", body.decode('utf-8'))
+    print("Body response:")
+    print("\t- type:", type(body))
+    print("\t- content:", body)
+    print("\t- utf8 content:", body.decode('utf-8'))
