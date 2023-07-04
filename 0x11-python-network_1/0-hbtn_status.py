9a854576b160bf62d1aec6a5670eb03a974b3691
akiyafaith <akiyafaith@gmail.com>
2023-07-04 14:35:09 -0700

    updated

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     url = 'https://intranet.hbtn.io/status'
 
     with urllib.request.urlopen(url) as response:
-        htm = response.read()
+        html = response.read()
 
     print("Body response:")
     print("\t- type: {type(html)}")
