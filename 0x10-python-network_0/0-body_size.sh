7b23ecd439cc92434b6f9054b9f40fe80e1b319f
akiyafaith <akiyafaith@gmail.com>
2023-07-05 05:56:13 -0700

    python network

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#  script that takes in a URL, sends a request to that URL, 
-# and displays the size of the body of the response
+#  script that takes in a URL, sends a request to that URL,and display size
 
 curl -s -w "%{size_download}" -o /dev/null "$1"
