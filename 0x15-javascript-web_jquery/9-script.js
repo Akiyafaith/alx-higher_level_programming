a4e9d7049b46a07996142996d704223b09846b11
akiyafaith <akiyafaith@gmail.com>
2023-08-02 21:52:08 +0000

    JQuery

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,4 +1,4 @@
-$('document').ready (fucntion () {
+$('document').ready (function () {
   $.get('https://fourtonfish.com/hellosalut/?lang=fr', function (data) {
     $('DIV#hello').text(data.hello);
   });
